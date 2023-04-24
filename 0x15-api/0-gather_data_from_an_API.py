dd3992bf38967bebc8ecbe718113c0386c6e765d
WankioM <111064857+WankioM@users.noreply.github.com>
2023-04-25 02:28:56 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
 """Takes an employee ID. And returns the to do list """
 
-import json
+import requests
 import sys
-import urllib.request
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    with urllib.request.urlopen(url + "users/{}".format(sys.argv[1])) as response:
-        user = json.loads(response.read().decode())
-
-    with urllib.request.urlopen(url + "todos?userId={}".format(sys.argv[1])) as response:
-        todos = json.loads(response.read().decode())
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed if True]
+    [print("\t {}".format(c)) for c in completed]
+
 
