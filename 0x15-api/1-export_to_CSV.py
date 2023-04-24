f26e69ba913065c816da4a8cd29751031fb372d6
WankioM <111064857+WankioM@users.noreply.github.com>
2023-04-25 02:12:55 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,21 +2,19 @@
 """Takes one employee info to csv"""
 
 import csv
-import json
+import requests
 import sys
-import urllib.request
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    with urllib.request.urlopen(url + "users/{}".format(user_id)) as response:
-        user = json.loads(response.read().decode())
+    user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    with urllib.request.urlopen(url + "todos?userId={}".format(user_id)) as response:
-        todos = json.loads(response.read().decode())
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
             [user_id, username, t.get("completed"), t.get("title")]
          ) for t in todos]
+
